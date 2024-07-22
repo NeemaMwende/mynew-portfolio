35db71fa9435398c848e1554d0c06f13fc95e5a2
NeemaMwende <gracepete12@gmail.com>
2024-07-22 20:29:14 +0300

    updates

---
+++ src/components/Background.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Particles from 'react-particles';
+import ParticlesConfig from '../components/config/ParticlesConfig'
+
+const Background = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Background;
